0da2cfd824bfdf623d510569b64b43b4e26c4739
QBN Kennel <qbnkennel@gmail.com>
2026-01-24 00:27:30 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,6 @@
+import React from 'react';
+import KibbleAnalyzer from './KibbleAnalyzer';
+
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-50 p-6">
-      <div className="max-w-4xl mx-auto">
-        <h1 className="text-3xl font-bold text-blue-600 mb-4">Kibble Analyzer</h1>
-        <p>Loading application...</p>
-      </div>
-    </div>
-  );
+  return <KibbleAnalyzer />;
 }
