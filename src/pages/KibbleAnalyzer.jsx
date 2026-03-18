842f86dac54d2469a8e4853d90a79113a0dbdc97
QBN Kennel <qbnkennel@gmail.com>
2026-03-18 19:05:35 +0000

    File changes

--- src/pages/KibbleAnalyzer.jsx
+++ src/pages/KibbleAnalyzer.jsx
@@ -1508,8 +1508,9 @@ Return up to 10 results with the most competitive prices. Include store name, pr
   }
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-50 p-6">
-      <div className="max-w-4xl mx-auto">
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-50">
+      <AnnouncementBanner />
+      <div className="max-w-4xl mx-auto p-6">
         <Card className="mb-8">
           <CardHeader>
             <div className="flex justify-between items-start gap-4">
